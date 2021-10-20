560bfeb5b54c213107ee800aecf970f25e2bc28d
Nasty <ozhgibesova.an@pit.local>
2021-10-20 15:16:26 +0500

    office commit

--- script/feel.js
+++ script/feel.js
@@ -1,49 +1,44 @@
-document.addEventListener("DOMContentLoaded", () =>{
-// Themes begin
+document.addEventListener("DOMContentLoaded", () => {
+    // Themes begin
 am4core.useTheme(am4themes_animated);
 // Themes end
 
-// Create chart instance
-var chart = am4core.create("feel_diagram", am4charts.XYChart3D);
+/* Create chart instance */
+var chart = am4core.create("feel_diagram", am4charts.RadarChart);
 
-// Add data
+/* Add data */
 chart.data = [{
-  "year": 0,
-  "income": 23.5,
-  "color": chart.colors.next()
+  "country": "Оптимизм",
+  "litres": 501
 }, {
-  "year": 5,
-  "income": 48.2,
-  "color": chart.colors.next()
+  "country": "Любовь",
+  "litres": 301
 }, {
-  "year": 10,
-  "income": 30.1,
-  "color": chart.colors.next()
+  "country": "Трепет",
+  "litres": 266
 }, {
-  "year": 15,
-  "income": 49.5,
-  "color": chart.colors.next()
+  "country": "Раскаяние",
+  "litres": 165
 }, {
-  "year": 20,
-  "income": 14.6,
-  "color": chart.colors.next()
+  "country": "Гнев",
+  "litres": 139
+}, {
+  "country": "Скука",
+  "litres": 336
 }];
 
-// Create axes
-var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-categoryAxis.dataFields.category = "year";
-categoryAxis.numberFormatter.numberFormat = "#";
-categoryAxis.renderer.inversed = true;
+/* Create axes */
+var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+categoryAxis.dataFields.category = "country";
 
-var  valueAxis = chart.xAxes.push(new am4charts.ValueAxis()); 
+var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+valueAxis.renderer.axisFills.template.fill = chart.colors.getIndex(2);
+valueAxis.renderer.axisFills.template.fillOpacity = 0.15;
 
-// Create series
-var series = chart.series.push(new am4charts.ColumnSeries3D());
-series.dataFields.valueX = "income";
-series.dataFields.categoryY = "year";
-series.name = "Income";
-series.columns.template.propertyFields.fill = "color";
-series.columns.template.tooltipText = "{valueX}";
-series.columns.template.column3D.stroke = am4core.color("#fff");
-series.columns.template.column3D.strokeOpacity = 0.2;
+/* Create and configure series */
+var series = chart.series.push(new am4charts.RadarSeries());
+series.dataFields.valueY = "litres";
+series.dataFields.categoryX = "country";
+series.name = "Sales";
+series.strokeWidth = 3;
 })
